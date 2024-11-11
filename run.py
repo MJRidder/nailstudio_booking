2c3e5c8656607215be1fbbdc6b4a969476999066
Maarten Ridder <maarten_ridder@hotmail.com>
2024-11-11 15:51:26 +0000

    added time choice function separately from booking function

--- run.py
+++ run.py
@@ -60,36 +60,29 @@ def validate_choice_client(choice):
             f"Unfortunately your provided answer '{choice}' is not one of the menu options. Please review the suggested answers above"
             )
 
-#def date_choice(values):
-#    """
-#    Uses the provided preferred date of the client and checks if this is available. 
-#    """
-#    dates_times = SHEET.worksheet("available_dates_times")
-#
-#    match_date = dates_times.findall(insert_date, in_column=1)
-
-
-
-
-
-def time_choice(choice):
-    choice = choice.strip().lower()
+def date_choice():
     """
-    Converts the provided A / B or C into the actual time that can be
-    matched with the worksheet.
+    Uses the provided preferred date of the client and checks if this is available. 
     """
-    if choice == "a":
-        time = choice.replace("a", "09:00")
-    elif choice == "b":
-        time = choice.replace("b", "10:00")
-    elif choice == "c":
-        time = choice.replace("c", "11:00")
-    else:
-        print(
-            f"Unfortunately your provided answer '{choice}' is not one of the menu options. Please review the suggested answers above"
-            )
+    dates_times = SHEET.worksheet("available_dates_times")
 
-    return time
+    insert_date = input("Please provide the desired date (in format: YEAR/MM/DD): ")
+
+    print("Checking if your chosen date is available...\n")
+
+    match_date = dates_times.findall(insert_date, in_column=1)
+
+    while match_date != "":
+
+        print(f"The requested date is available.\n")
+        #print(match_date)
+        return match_date
+        break
+    
+    print("The requested date is not availble, please choose again a date that falls in a week day.")
+    date_choice()
+
+
 
 def get_date_rows(choices):
     """
@@ -115,6 +108,43 @@ def get_date_rows(choices):
     return(available_times)
 
 
+def time_choice(choice):
+    choice = choice.strip().lower()
+    """
+    Converts the provided A / B or C into the actual time that can be
+    matched with the worksheet.
+    """
+    if choice == "a":
+        time = choice.replace("a", "09:00")
+    elif choice == "b":
+        time = choice.replace("b", "10:00")
+    elif choice == "c":
+        time = choice.replace("c", "11:00")
+    else:
+        print(
+            f"Unfortunately your provided answer '{choice}' is not one of the menu options. Please review the suggested answers above"
+            )
+
+    return time
+
+def time_availability(choice):
+    """
+    Converts the provided A / B or C into the actual time that can be
+    matched with the worksheet.
+    """
+    dates_times = SHEET.worksheet("available_dates_times")
+
+    available_time1 = SHEET.worksheet("available_dates_times").acell(choice[0]).value
+    available_time2 = SHEET.worksheet("available_dates_times").acell(choice[1]).value
+    available_time3 = SHEET.worksheet("available_dates_times").acell(choice[2]).value
+
+    print(f"Please choose below from the following available time(s) on that date: {available_time1} {available_time2} {available_time3}\n")
+
+    insert_time = input("A = 09:00 | B = 10:00 | C = 11:00 : ")
+
+    time_choice(insert_time)
+    print(f"You have chosen {time_choice} as your desired time.")
+
 def book_appointment():
     """
     Ensure that a correct format of date is given and entered by the client.
@@ -125,27 +155,25 @@ def book_appointment():
     print("Please first provide us with the date you'd like to book using the following format: (YEAR/MM/DD).")
     print("If that date is available, you can then choose one of the available time slots.")
     print("Please share your name and your contact number in case we need to reach you.")
-    print("After this your booking is complete and you will receive your unique booking number.")
+    print("After this your booking is complete and you will receive your unique booking number.\n")
 
     dates_times = SHEET.worksheet("available_dates_times")
     
-    insert_date = input("Please provide the desired date (in format: YEAR/MM/DD): ")
+#    insert_date = input("Please provide the desired date (in format: YEAR/MM/DD): ")
+#
+#    print("Checking if your chosen date is available...\n")
+#
+    date_check = date_choice()
+    #print(date_check)
 
-    print("Checking if your chosen date is available...\n")
-
-    #date_choice(insert_date)
-
-    match_date = dates_times.findall(insert_date, in_column=1) #separate function?
-    time_cells = get_date_rows(match_date)
+    time_cells = get_date_rows(date_check)
     #print(time_cells)
 
-    available_time1 = SHEET.worksheet("available_dates_times").acell(time_cells[0]).value
-    available_time2 = SHEET.worksheet("available_dates_times").acell(time_cells[1]).value
-    available_time3 = SHEET.worksheet("available_dates_times").acell(time_cells[2]).value
+    time_availability(time_cells)
 
-    print(f"Your requested date is available, please choose below from the following available time(s) on that date: {available_time1} {available_time2} {available_time3}\n")
+    time_availability()
 
-    insert_time = input("A = 09:00 | B = 10:00 | C = 11:00 : ")
+
 
 
 def edit_appointment():
